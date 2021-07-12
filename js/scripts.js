ed84df1352b2194a596322effa75a19852bad8bc
ramoshe <rsherard@mac.com>
2021-07-12 14:32:24 -0400

    separate functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,69 @@
 const gallery = document.querySelector('#gallery');
 
-fetch('https://randomuser.me/api/?nat=us&results=12')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.results);
-        displayUsers(data.results);
-    });
+const usersPromise = new Promise( (resolve, reject) => {
+    fetch('https://randomuser.me/api/?nat=us&results=12')
+        .then(response => response.json())
+        .then(data => resolve(data.results));
+});
 
-function displayUsers(data) {
-    data.forEach(person => {
-        const userCard = document.createElement('div');
-        userCard.className = 'card';
-        userCard.insertAdjacentHTML('beforeend', `
-            <div class="card-img-container">
-                <img class="card-img" src="${person.picture.large}" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-                <p class="card-text">${person.email}</p>
-                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
-            </div>
-        `);
-        gallery.insertAdjacentElement('beforeend', userCard);
+usersPromise
+    .then(users => users.forEach((person, index) => generateUser(person, index)));
 
-        const userModal = document.createElement('div');
-        const userDOB = new Date(person.dob.date);
-        userModal.className = 'modal-container';
-        userModal.insertAdjacentHTML('beforeend', `
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${person.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-                    <p class="modal-text">${person.email}</p>
-                    <p class="modal-text cap">${person.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${person.cell}</p>
-                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${userDOB.getMonth()}/${userDOB.getDay()}/${userDOB.getFullYear()}</p>
-                </div>
-            </div>
-            <div class="modal-btn-container">
-                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                <button type="button" id="modal-next" class="modal-next btn">Next</button>
-            </div>
-        `);
-        userCard.addEventListener('click', () => {
-            gallery.insertAdjacentElement('afterend', userModal);
-            document.querySelector('#modal-close-btn').addEventListener('click', (e) => {
-                userModal.remove();
-            });
+function generateUser(person, index) {
+    const userCard = document.createElement('div');
+    userCard.className = 'card';
+    userCard.insertAdjacentHTML('beforeend', `
+        <div class="card-img-container">
+            <img class="card-img" src="${person.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+            <p class="card-text">${person.email}</p>
+            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        </div>
+    `);
+    gallery.insertAdjacentElement('beforeend', userCard);
+    userCard.addEventListener('click', () => {
+        const userModal = generateModal(person);
+        gallery.insertAdjacentElement('afterend', userModal);
+        document.querySelector('#modal-close-btn').addEventListener('click', (e) => {
+            userModal.remove();
+        });
+        document.querySelector('#modal-next').addEventListener('click', (e) => {
+            console.log('next');
+        });
+        document.querySelector('#modal-prev').addEventListener('click', (e) => {
+            console.log('prev');
         });
     });
+}
+
+function generateModal(person) {
+    const userModal = document.createElement('div');
+    const userDOB = new Date(person.dob.date);
+    userModal.className = 'modal-container';
+    userModal.insertAdjacentHTML('beforeend', `
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${person.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.cell}</p>
+                <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+                <p class="modal-text">Birthday: ${userDOB.getMonth()}/${userDOB.getDay()}/${userDOB.getFullYear()}</p>
+            </div>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    `);
+    return userModal;
+}
+
+function buttonHandler() {
+    // eventually move event listener stuff here
 }
